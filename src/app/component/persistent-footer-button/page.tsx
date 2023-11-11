ed4314e3d0005a51e4e4fa6339642d04d840488f
Alex <contact@alex-min.fr>
2023-11-11 13:14:57 +0100

    better mobile view

--- src/app/component/persistent-footer-button/page.tsx
+++ src/app/component/persistent-footer-button/page.tsx
@@ -1,17 +1,6 @@
 'use client';
-import { LivePad } from '../../components/live-pad';
-import { Menu } from '../../components/menu';
-import { TopBar } from '../../components/top-bar';
+import Home from '../../page';
 
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-slate-600">
-      <TopBar />
-      <div className="flex flex-row w-full">
-        <Menu />
-        <LivePad preload='PersistentFooterButton' />
-      </div>
-    </main>
-  )
+export default function Page() {
+  return <Home preload="PersistentFooterButton" />;
 }
